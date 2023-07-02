bcfa6171e4cbba7c0e9476654bc089ceeb54d312
biratdatta <birat.datta26@gmail.com>
2023-07-02 12:32:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const puppeteer = require('puppeteer');
+
+async function run(){
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.biratdatta.live/');
+
+    await page.screenshot({ path: 'example.png', fullPage: true});
+await page.pdf({ path: 'example.pdf', format: 'A4'});
+
+    await browser.close();
+}
+
+run();
